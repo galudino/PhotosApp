b066ddc346e25380f1ac402effb72b9137a75c54
Patrick Nogaj <patricknogaj@gmail.com>
2019-03-10 01:32:47 -0500

    added some updates to user.

--- src/model/Photo.java
+++ src/model/Photo.java
@@ -1,3 +1,14 @@
+/**
+ * Photo.java
+ *
+ * Copyright (c) 2019 Gemuele Aludino, Patrick Nogaj. 
+ * All rights reserved.
+ *
+ * Rutgers University: School of Arts and Sciences
+ * 01:198:213 Software Methodology, Spring 2019
+ * Professor Seshadri Venugopal
+ */
+
 package model;
 
 public class Photo {
--- src/model/User.java
+++ src/model/User.java
@@ -1,5 +1,33 @@
+/**
+ * User.java
+ *
+ * Copyright (c) 2019 Gemuele Aludino, Patrick Nogaj. 
+ * All rights reserved.
+ *
+ * Rutgers University: School of Arts and Sciences
+ * 01:198:213 Software Methodology, Spring 2019
+ * Professor Seshadri Venugopal
+ */
+
 package model;
 
-public class User {
+import java.io.Serializable;
 
+public class User implements Comparable<User>, Serializable {
+
+	private String username;
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String toString() {
+		return username;
+	}
+
+	@Override
+	public int compareTo(User user) {
+		return this.username.compareToIgnoreCase(user.username);
+	}
+	
 }
