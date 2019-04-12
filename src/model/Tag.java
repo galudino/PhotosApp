3005f14d4e555fbc26ddf035d4f8e22f033b856b
Patrick Nogaj <patricknogaj@gmail.com>
2019-04-12 16:56:34 -0400

    Implemented tag object.

--- src/model/Tag.java
+++ src/model/Tag.java
@@ -1,9 +1,65 @@
+/**
+ * Tag.java
+ *
+ * Copyright (c) 2019 Gemuele Aludino, Patrick Nogaj. 
+ * All rights reserved.
+ *
+ * Rutgers University: School of Arts and Sciences
+ * 01:198:213 Software Methodology, Spring 2019
+ * Professor Seshadri Venugopal
+ */
+
 package model;
 
-public class Tag {
+import java.io.Serializable;
 
+public class Tag implements Comparable<Tag>, Serializable {
+	
+	private static final long serialVersionUID = 176039707375704571L;
+	private String tagName;
+	private String tagValue;
+
+	/**
+	 * Constructor that tags a tagName and tagValue field and sets it to the private fields.
+	 */
+	public Tag(String tagName, String tagValue) {
+		this.tagName = tagName;
+		this.tagValue = tagValue;
+	}
+	
+	/**
+	 * @param takes a Tag object and sets the fields accordingly.
+	 */
 	public Tag(Tag t) {
-		// TODO Auto-generated constructor stub
+		tagName = t.tagName;
+		tagValue = t.tagValue;
+	}
+	
+	/**
+	 * @return tagName field
+	 */
+	public String getTagName() { 
+		return tagName;
+	}
+	
+	/**
+	 * @return tagValue field
+	 */
+	public String getTagValue() { 
+		return tagValue;
+	}
+	
+	public String toString() {
+		return tagName + " :: " + tagValue;
+	}
+
+	/**
+	 * @param Takes in a tag object to compare with
+	 * return's a value based to see if the tag names are equal, if false it will compare tag values.
+	 */
+	@Override
+	public int compareTo(Tag other) {
+		return (this.tagName.compareTo(other.tagName) != 0) ? this.tagName.compareTo(other.tagName) : this.tagValue.compareToIgnoreCase(other.tagValue);
 	}
 
 }
