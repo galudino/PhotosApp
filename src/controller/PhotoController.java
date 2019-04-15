56256da80f937d8796414a8b95008efe12616ec5
Gem Aludino <g.aludino@gmail.com>
2019-04-14 20:32:52 -0400

    Navigator button methods and zoom lever methods -- may need more work

--- src/controller/PhotoController.java
+++ src/controller/PhotoController.java
@@ -14,16 +14,26 @@ package controller;
 import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Slider;
 import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Modality;
@@ -31,6 +41,7 @@ import javafx.stage.Stage;
 import model.Album;
 import model.Photo;
 import model.PhotoModel;
+import model.Tag;
 
 /**
  * Controller class to manage functionality of import.fxml
@@ -40,19 +51,52 @@ import model.PhotoModel;
  */
 public class PhotoController {
 	
-	public PhotoModel model = LoginController.getModel();
+	int index = 0;
 	
-	// TODO fields
+	final double DEFAULT_INSET_VALUE = 10.0;
+	final double DEFAULT_HEIGHT_VALUE = 150.0;
+	final double DEFAULT_WIDTH_VALUE = 200.0;
+		
+	double imageInsetValue = DEFAULT_INSET_VALUE;
+	double imageHeightValue = DEFAULT_HEIGHT_VALUE;
+	double imageWidthValue = DEFAULT_WIDTH_VALUE;
+	
+	ObservableList<Photo> photoList = FXCollections.observableArrayList();
+	
+	private List<ImageView> currentImageViewList = null;
+	
+	public PhotoModel model = LoginController.getModel();
 	
 	ObservableList<Photo> pList = FXCollections.observableArrayList();
 	
+	//@formatter:off
 	@FXML ListView<Photo> imageQueueList;
 	@FXML SplitMenuButton albumList;
+	
+	@FXML Label sizeField;
+	@FXML Label createdField;
+	@FXML Label pathField;
+	@FXML Label nameField;
+	
+	@FXML TextField captionField;
+	@FXML Label displayCaption;
+	@FXML ListView<Tag> tagList;
+	
+	@FXML ImageView detailView;
+	
+	@FXML Button navigatorButtonBack;
+	@FXML Button navigatorButtonNext;
+	
+	@FXML Slider zoomSlider;
+	//@formatter:on
 	
 	FileChooser fileChooser;
 	File file;
 	Desktop desktop = Desktop.getDesktop();
 	
+	private Photo currentPhoto = null;
+	private Album currentAlbum = null;
+		
 	@FXML
 	public void initialize() {
 		/**
@@ -60,6 +104,280 @@ public class PhotoController {
 		 */
 		System.out.println();
 		debugLog("Entering " + getClass().getSimpleName());
+	}
+	
+	private void setSelectedIndex(int selectedIndex) {
+		index = selectedIndex;
+	}
+	
+	public void doZoomSlider() {
+		if (currentAlbum == null) {
+			return;
+		}
+		
+		zoomSlider.valueProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable,
+					Number oldValue, Number newValue) {
+				double factor = newValue.doubleValue();
+				
+				imageInsetValue = factor * DEFAULT_INSET_VALUE; // default 10.00
+				imageHeightValue = factor * DEFAULT_HEIGHT_VALUE; // default 150.00
+				imageWidthValue = factor * DEFAULT_WIDTH_VALUE; // default 200.00
+				
+				for (ImageView iv : currentImageViewList) {
+					iv.setFitHeight(imageHeightValue);
+					iv.setFitWidth(imageWidthValue);
+					iv.setPreserveRatio(true);
+				}
+
+				debugLog("" + newValue.doubleValue());
+				
+				debugLog(imageInsetValue + " inset");
+				debugLog(imageHeightValue + " height");
+				debugLog(imageWidthValue + " width");
+			}
+			
+		});
+	}
+	
+	/**
+	 * Executes upon activating navigator back button
+	 */
+	public void doNavigatorButtonBack() {
+		/**
+		 * No album was selected.
+		 */
+		if (currentAlbum == null) {
+			return;
+		}
+		
+		/**
+		 * Album is selected but photo is not selected
+		 */
+		if (currentAlbum != null && currentPhoto == null) {
+			return;
+		}
+		
+		/**
+		 * Album is selected and has at least 1 photo
+		 */
+		if (currentAlbum.getAlbumSize() != 0 && currentAlbum != null) {
+
+			System.out.println(currentAlbum.getAlbumSize());
+			/**
+			 * Establish new index. If current index equals 0, set the index to
+			 * the last index of the currentImageViewList. (loop to end)
+			 * 
+			 * Otherwise, decrement index by one to go the previous image.
+			 */
+			ImageView old;
+			ImageView iv;
+
+			if (index == 0) {
+				old = currentImageViewList.get(index);
+				index = currentImageViewList.size() - 1;
+				iv = currentImageViewList.get(index);
+			} else {
+				old = currentImageViewList.get(index);
+				--index;
+				iv = currentImageViewList.get(index);
+			}
+
+			/**
+			 * Add an event filter for navigatorButtonBack, for when the primary
+			 * mouse button is pressed.
+			 */
+			navigatorButtonBack.addEventFilter(MouseEvent.MOUSE_PRESSED, e -> {
+				if (e.isPrimaryButtonDown()) {
+					debugLog(
+							"[navigatorButtonBack] Selected index field reads: "
+									+ index);
+
+					old.setStyle(null);
+					if (iv.getBoundsInParent() != null) {
+						iv.setStyle(
+								"-fx-effect: innershadow(gaussian, #039ed3, 4, 2.0, 0, 0);");
+					}
+
+					/**
+					 * Retrieve the current photo based on the current index
+					 */
+					currentPhoto = photoList.get(index);
+
+					/**
+					 * Update selected image in detail pane
+					 */
+					detailView.setImage(
+							currentImageViewList.get(index).getImage());
+					detailView.setFitHeight(150);
+					detailView.setFitWidth(200);
+					detailView.setVisible(true);
+					detailView.setPreserveRatio(true);
+
+					/**
+					 * Update the caption section.
+					 */
+					displayCaption.setText(currentPhoto.getCaption());
+					captionField.setText("");
+
+					/**
+					 * Update selected image's detail pane
+					 */
+					nameField.setText(currentPhoto.getFilename());
+					pathField.setText(currentPhoto.getFilepath());
+					sizeField.setText("(size)" + " KB");
+					createdField.setText(currentPhoto.getDatePhoto());
+
+					ObservableList<Tag> tList = FXCollections
+							.observableArrayList();
+
+					/**
+					 * Update tags in image detail pane
+					 */
+					for (Map.Entry<String, Tag> tag : currentPhoto.getTagMap()
+							.entrySet()) {
+						tList.add(tag.getValue());
+					}
+
+					currentPhoto.setTagList(tList);
+					tagList.setItems(tList);
+
+				}
+			});
+
+			/**
+			 * CONSOLE DIAGNOSTICS
+			 */
+			debugLog("[navigatorButtonBack] Image " + currentPhoto.getFilename()
+					+ " was selected");
+
+			/**
+			 * CONSOLE DIAGNOSTICS
+			 */
+			debugLog("Clicked navigator button back");
+		} else {
+
+		}
+	}
+
+	/**
+	 * Executes upon activating navigator next button
+	 */
+	public void doNavigatorButtonNext() {
+		/**
+		 * No album was selected
+		 */
+		if (currentAlbum == null) {
+			return;
+		}
+		
+		/**
+		 * Album is selected but photo is not selected
+		 */
+		if (currentAlbum != null && currentPhoto == null) {
+			return;
+		}
+
+		/**
+		 * Album is selected and has at least 1 photo
+		 */
+		if (currentAlbum.getAlbumSize() != 0 && currentAlbum != null) {
+			/**
+			 * Establish new index. If current index equals the
+			 * currentImageViewList size, minus 1 (new index == last index),
+			 * then set the index to zero. (loop to beginning)
+			 * 
+			 * Otherwise, increment index by one to go to the next image.
+			 */
+			ImageView old;
+			ImageView iv;
+
+			if (index == currentImageViewList.size() - 1) {
+				old = currentImageViewList.get(index);
+				index = 0;
+				iv = currentImageViewList.get(index);
+			} else {
+				old = currentImageViewList.get(index);
+				++index;
+				iv = currentImageViewList.get(index);
+			}
+
+			/**
+			 * Add an event filter for navigatorButtonNext, for when the primary
+			 * mouse button is pressed.
+			 */
+			navigatorButtonNext.addEventFilter(MouseEvent.MOUSE_PRESSED, e -> {
+				if (e.isPrimaryButtonDown()) {
+					debugLog(
+							"[navigatorButtonNext] Selected index field reads: "
+									+ index);
+
+					old.setStyle(null);
+					if (iv.getBoundsInParent() != null) {
+						iv.setStyle(
+								"-fx-effect: innershadow(gaussian, #039ed3, 4, 2.0, 0, 0);");
+					}
+
+					/**
+					 * Retrieve the current photo based on the current index
+					 */
+					currentPhoto = photoList.get(index);
+
+					/**
+					 * Update selected image in detail pane
+					 */
+					detailView.setImage(
+							currentImageViewList.get(index).getImage());
+					detailView.setFitHeight(150);
+					detailView.setFitWidth(200);
+					detailView.setVisible(true);
+					detailView.setPreserveRatio(true);
+
+					/**
+					 * Update the caption section.
+					 */
+					displayCaption.setText(currentPhoto.getCaption());
+					captionField.setText("");
+
+					/**
+					 * Update selected image's detail pane
+					 */
+					nameField.setText(currentPhoto.getFilename());
+					pathField.setText(currentPhoto.getFilepath());
+					sizeField.setText("(size)" + " KB");
+					createdField.setText(currentPhoto.getDatePhoto());
+
+					ObservableList<Tag> tList = FXCollections
+							.observableArrayList();
+
+					/**
+					 * Update tags in image detail pane
+					 */
+					for (Map.Entry<String, Tag> tag : currentPhoto.getTagMap()
+							.entrySet()) {
+						tList.add(tag.getValue());
+					}
+
+					currentPhoto.setTagList(tList);
+					tagList.setItems(tList);
+
+				}
+			});
+
+			/**
+			 * CONSOLE DIAGNOSTICS
+			 */
+			debugLog("[navigatorButtonNext] Image " + currentPhoto.getFilename()
+					+ " was selected");
+
+			/**
+			 * CONSOLE DIAGNOSTICS
+			 */
+			debugLog("Clicked navigator button next");
+		}
+
 	}
 	
 	public void selectFile() {
